4907fbfde5ec09ca685817445d29139943192351
casas91 <casas_91@hotmail.com>
2023-10-26 13:28:53 +0200

    actualizacion

--- Ejercicios Dia 3/Persona/src/Main.java
+++ Ejercicios Dia 3/Persona/src/Main.java
@@ -3,7 +3,7 @@ public class Main {
 
         //Creacion del vector persona.
 
-        Persona vectorpersona[] = new Persona[5];
+        Persona [] vectorpersona = new Persona[5];
 
         //Creacion de personas.
 
@@ -13,19 +13,30 @@ public class Main {
         vectorpersona[3] = new Persona(4, "Marta", 52, "Calle 7", "452375");
         vectorpersona[4] = new Persona(5, "Judith", 35, "Calle 1", "545247");
 
-        //Recorremos el vector
 
-        for (int i = 0; i < vectorpersona.length; i++) {
+        //Recorremos el vector y mostramos nombre y edad por pantalla.
+        int i;
+        for (i = 0; i < vectorpersona.length; i++) {
+            System.out.println("Nombre: " + vectorpersona[i].getNombre() + "  Edad: " + vectorpersona[i].getEdad());
+        }
 
-            //Mostramos nombre y edad por pantalla
-            //System.out.println("Nombre: " + vectorpersona[i].getNombre() + "  Edad: " + vectorpersona[i].getEdad());
+        // Cambiar el nombre de dos personas y mostrar antes y después
+        vectorpersona[0].setNombre("Pedro");
+        vectorpersona[2].setNombre("Camila");
 
-            //Mostrar por pantalla a las personas mayores de 30 años
-            if (vectorpersona[i].getEdad() > 30) {
+        System.out.println("Nombres antes y después del cambio:");
+        System.out.println("Nombre antes del cambio: " + "David");
+        System.out.println("Nombre después del cambio: " + vectorpersona[0].getNombre());
+        System.out.println("Nombre antes del cambio: " + "Maria");
+        System.out.println("Nombre después del cambio: " + vectorpersona[2].getNombre());
 
-                System.out.println("ID: " + vectorpersona[i].getId() + "  Nombre: " + vectorpersona[i].getNombre() + "  Edad: " + vectorpersona[i].getEdad() + " Direccion: " + vectorpersona[i].getDireccion() +" Numero de telefono: "+vectorpersona[i].getNumTelefono());
-            }
-            }
+        //Mostrar por pantalla a las personas mayores de 30 años
+        System.out.println("Personas mayores de 30 años");
+        for (i = 0; i < vectorpersona.length; i++){
+        if (vectorpersona[i].getEdad() > 30) {
+            System.out.println("ID: " + vectorpersona[i].getId() + "  Nombre: " + vectorpersona[i].getNombre() + "  Edad: " + vectorpersona[i].getEdad() + " Direccion: " + vectorpersona[i].getDireccion() + " Numero de telefono: " + vectorpersona[i].getNumTelefono());
+        }
+    }
 
 
         }
